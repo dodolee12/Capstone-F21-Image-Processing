d90824704fb5a35a3b18733e935658e6a0a2378c
chao <zichao2018@gmail.com>
2021-12-16 12:04:31 -0500

    update

--- Drawing.py
+++ Drawing.py
@@ -1,12 +1,14 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.graphics import Color, Ellipse, Line
 import drawfile
 from send_commands_through_bluetooth import send_coordinates, address
 import asyncio
 from kivy.core.window import Window
-Window.size = (500, 600)
+Window.size = (500, 700)
+Window.clearcolor = (1, 1, 1, 1)
 
 class MyPaintWidget(Widget):
     def __init__(self):
@@ -16,8 +18,9 @@ class MyPaintWidget(Widget):
         super().__init__()
 
     def on_touch_down(self, touch):
+        if (touch.x < 0 or touch.x > 500 or touch.y < 0 or touch.y > 600): return
         with self.canvas:
-            Color(1, 1, 0)
+            Color(0,0,0)
             touch.ud['line'] = Line(points=(touch.x, touch.y))
             self.down_x = touch.x
             self.down_y = touch.y
@@ -26,18 +29,20 @@ class MyPaintWidget(Widget):
             f.write("({},{})\n".format(round(touch.x), round(touch.y)))
 
     def on_touch_move(self, touch):
+        if (touch.x < 0 or touch.x > 500 or touch.y < 0 or touch.y > 600): return
         touch.ud['line'].points += [touch.x, touch.y]
         f = open('out.log', 'a')
         f.write("({},{})\n".format(round(touch.x), round(touch.y)))
         print(touch.x, touch.y)
 
     def on_touch_up(self, touch):
+        if (touch.x < 0 or touch.x > 500 or touch.y < 0 or touch.y > 600): return
         if(pow(touch.x - self.down_x,2) + pow(touch.y - self.down_y,2) > 20):
             print('ok')
             return
 
         with self.canvas:
-            Color(1, 1, 0)
+            Color(0,0,0)
             d = 10.
             Ellipse(pos=(touch.x - d / 2, touch.y - d / 2), size=(d, d))
             f = open('out.log', 'a')
@@ -52,16 +57,21 @@ class MyPaintApp(App):
         self.painter = MyPaintWidget()
 
         # clear btn
-        clearbtn = Button(text='Clear', pos =(200, 0))
+        clearbtn = Button(text='Clear', pos =(300, 600))
         clearbtn.bind(on_release=self.clear_canvas)
 
         # send button
-        sendbtn = Button(text='Sendout', pos = (100, 0))
+        sendbtn = Button(text='Sendout', pos = (400, 600))
         sendbtn.bind(on_release=self.send_out)
+
+        l = Label(text='[b]Triple Tangent \n Project 2022[/b]', markup=True, font_size=30, color=(0,0,0), pos=(100,600))
+
 
         parent.add_widget(self.painter)
         parent.add_widget(clearbtn)
         parent.add_widget(sendbtn)
+        parent.add_widget(l)
+
 
         self.clear_canvas("obj")
 
